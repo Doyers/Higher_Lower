86fa2e54fad0e669b9a25612c99196860be177eb
Alan <alan15-7@hotmail.com>
2018-09-17 13:15:20 +0200

    First Commit, Set-up project

---
+++ app/src/main/java/com/twelker/higher_lower_assignment/Higher_Lower_Activity.java
@@ -0,0 +1,13 @@
+package com.twelker.higher_lower_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Higher_Lower_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_higher__lower_);
+    }
+}
